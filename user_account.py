c0be073bfcd8f37faa96b2f81e881c090aca7ba0
b.r <mahdis.taghizadeh1376@gmail.com>
2021-12-14 00:46:48 +0330

    modular programming.This is an initial code and must be optimized.

---
+++ user_account.py
@@ -0,0 +1,38 @@
+import logging
+import re
+import hashlib
+import file_handler_module
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('user.log')
+file_handler.setLevel(level=logging.INFO)
+
+# create formatters and add it to handlers
+
+file_format = logging.Formatter('%(asctime)s ::%(levelname)s - %(filename)s - %(message)s')
+file_handler.setFormatter(file_format)
+
+# add handlers to the logger
+logger.addHandler(file_handler)
+
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def sign_up(self):
+        # if his/her username not exist, he/she can sign up .
+        logger.info('a new person singed up into program')
+        return " register a person into program"
+
+    def sign_in(self):
+        # if the username and password is correct,the user can sign in .
+        logger.info('an user was signed in program ')
+        return "sign in a person into program"
+
+
+if __name__ == '__main__':
+    User()
