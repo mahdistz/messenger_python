b7918c73f2440c1316a7e96f2f17c675d6fc396f
b.r <mahdis.taghizadeh1376@gmail.com>
2021-12-16 00:06:07 +0330

    changes somewhere of code ant try to complete it!

--- main.py
+++ main.py
@@ -1,37 +1,98 @@
 import user_account
+import working_in_messenger
+import file_handling
 import logging
-
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('main.log')
-file_handler.setLevel(level=logging.INFO)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(level=logging.INFO)
+file_handler = logging.FileHandler('user.log')
+file_handler.setLevel(level=logging.WARNING)
 
 # create formatters and add it to handlers
 
+stream_format = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_format = logging.Formatter('%(asctime)s ::%(levelname)s - %(filename)s - %(message)s')
+stream_handler.setFormatter(stream_format)
 file_handler.setFormatter(file_format)
 
 # add handlers to the logger
+
+logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
+
+def get_item_for_register():
+    validation = False
+    print('username must be 3 to 12 characters and can include letters,numbers and "-" ,"." ,"_" ')
+    print("Password must be 6 to 12 characters,1 lowercase letter,1 uppercase letter,1 digit")
+    username_input1 = input('Enter username: ')
+    password_input1 = input('Enter password: ')
+    an_user = user_account.User(username_input1, password_input1)
+    if an_user.validation_username():
+        logger.info(f'this username {username_input1} is correct')
+        if an_user.validation_password():
+            again_password = input('repeat password : ')
+            if password_input1 == again_password:
+                print('you successfully added to Messenger.')
+                validation = True
+            else:
+                logger.error('The password you entered does not match the default password.')
+                print('You can try up to 3 times')
+                for i in range(3):
+                    again_password_2 = input('repeat password : ')
+                    if again_password_2 == password_input1:
+                        print('you successfully added to Messenger.')
+                        validation = True
+                    else:
+                        print('sorry you can not try anymore! ')
+                        break
+    else:
+        logger.info('this username is not correct! please try again...')
+
+    return validation, username_input1, password_input1
+
+
 """
-register
+Menu
 """
-# An user register in messenger
 
-user_name = input("Enter username: ")
-user_password = input("Enter password: ")
+while True:
+    try:
+        my_input = input('Welcome to Messenger.Do you have an Account?(y/n): ').lower()
+        if my_input == 'y':
+            username_input = input('Enter username: ')
+            password_input = input('Enter password: ')
+            an_user1 = user_account.User(username_input, password_input)
+            an_user1.login()
 
-# if username is unique in this program and username and password patterns is correct
-# then can be registered
+        elif my_input == 'n':
 
-user_defined_username = user_account.User(user_name, user_password).validation_username()
-user_defined_password = user_account.User(user_name, user_password).validation_password()
+            creating_account = input('Do You Want Sign up?(y/n): ').lower()
+            if creating_account == 'y':
+                items_tuple = get_item_for_register()
+                # item_tuple = (True/False, username , password)
+                if items_tuple[0]:
+                    user = user_account.User(items_tuple[1], items_tuple[2])
+                    user.register()
+                    # print(user)
+            elif creating_account == 'n':
+                wanting_quit = input('Do You Want Quit?(y/n): ').lower()
+                if wanting_quit == 'y':
+                    break
+                elif wanting_quit == 'n':
+                    continue
+                else:
+                    logger.error('you should enter y or n')
+            else:
+                logger.error('you should enter y or n')
+        else:
+            logger.error('you should enter y or n')
 
-if user_defined_password and user_defined_username:
-    user_account.User(user_name, user_password).register()
+    except (ValueError, TypeError) as error:
+        logger.error('an exception occurred', exc_info=True)
 
-"""
-login
-"""
+    except Exception as error:
+        logger.error('an exception occurred', exc_info=True)
--- user_account.py
+++ user_account.py
@@ -34,10 +34,10 @@ class User:
         self.__password = password
 
     def register(self):
-        # for each user ,username and password must be saved in file.
+        # for each user ,username and password must be saved in file
         # if his/her username not exist, he/she can sign up .
         # information_list = [{'key':'value'},...]
-        my_file = file_handling.File('users.csv')
+        my_file = file_handling.File('users_information.csv')
         information_list = my_file.read_csvfile_as_dictionary()
         usernames_list = []
         for item in information_list:
@@ -46,35 +46,64 @@ class User:
         if self.username not in usernames_list:
 
             info = {'username': self.username, 'password': User.hash_method(self.__password)}
-            file_handling.File('users.csv').write(info)
+            file_handling.File('users_information.csv').write(info)
             logger.info(f" new person with username: {self.username},registered into program")
         else:
             logging.warning("this username isn't available")
 
+    @staticmethod
+    def get_users_info_list():
+        """
+        a list of tuples >> [(username,password)]
+        """
+        my_file = file_handling.File('users_information.csv')
+        information_list = my_file.read_csvfile_as_dictionary()
+        users_list = []
+        for item in information_list:
+            users_list.append((item['username'], item['password']))
+        return users_list
+
     def login(self):
         # if the username and password is correct,the user can sign in .
-        logger.info('an user was signed in program ')
+
+        info_list = User.get_users_info_list()
+        usernames_list = info_list[0]
+        print(usernames_list)
+
+        if self.username not in usernames_list:
+            logger.error('this username not exist!')
+        else:
+            for item in info_list:
+                if item[self.username] == User.hash_method(self.__password):
+                    logger.info(f'You: {self.username} have successfully entered the program :) ')
+                else:
+                    logger.error('the password is not correct.try again')
         return "sign in a person into program"
 
     def validation_password(self):
-        """   Password must be
-            - 6 chars or more
-            - 1 lowercase letter
-            - 1 uppercase letter
-            - 1 digit
-        """
+        # Password must be 6 to 12 characters, 1 lowercase letter
+        # 1 uppercase letter,1 digit
         return PASSWORD_REGEX.match(self.__password) is not None
 
     def validation_username(self):
+        # username must be 3 to 12 characters and can include
+        # letters,numbers and "-" ,"." ,"_"
         return USERNAME_REGEX.match(self.username) is not None
-
-    def check_username_with_password(self):
-        pass
 
     @staticmethod
     def hash_method(obj):
         obj_hashing = sha3_256(obj.encode('ascii')).hexdigest()
         return obj_hashing
 
+    def check_hash_items(self, password1):
+        password1 = User.hash_method(password1)
+        password = User.hash_method(self.__password)
+        if password1 == password:
+            return True
+        return False
+
     def __repr__(self):
         return f'username: {self.username},password(hashing): {User.hash_method(self.__password)}'
+
+# print(User.login())
+print(User.get_users_info_list())
