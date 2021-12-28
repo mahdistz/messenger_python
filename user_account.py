9535493da9be5734e76a0533b695d06583afd262
b.r <mahdis.taghizadeh1376@gmail.com>
2021-12-28 16:39:12 +0330

    work on lock and unlock methods

--- user_account.py
+++ user_account.py
@@ -30,23 +30,27 @@ class User:
         information_list -->> a list of dictionaries of 'username and password(hashing)' of each user
         users_list -->> list of usernames
         passwords_list -->> list of passwords(hashing)
-        :return: ([{'username':...,'password(hash)':...}],[usernames],[passwords])
+        locking_list -->> list of self.Lock
+        :return: ([{'username':...,'password(hash)':...}],[usernames],[passwords],[locking])
         """
         # usernames list
         users_list = []
         # password list
         passwords_list = []
+        # locking list
+        locking_list = []
         my_file = File('users_information.csv')
         if os.path.exists('users_information.csv'):
             information_list = my_file.read_csvfile_as_dictionary()
             for item in information_list:
                 users_list.append(item['username'])
                 passwords_list.append(item['password'])
+                locking_list.append(item['locking'])
         else:
             # creating csv file
-            my_file.write({'username': '', 'password': ''})
+            my_file.write({'username': '', 'password': '', 'locking': ''})
             information_list = []
-        return information_list, users_list, passwords_list
+        return information_list, users_list, passwords_list, locking_list
 
     def quit_from_messenger(self):
         logger.info(f'{self.username} log out from messenger')
@@ -119,56 +123,58 @@ class Login(User):
         # if a user enter incorrect password for 3 time then his/her account
         # locked for one hour
         self.locking = True
+        df = pd.read_csv('users_information.csv')
+        index_list = df.index[(df['username'] == self.username)].tolist()
+        index = int(index_list[0])
+        df.loc[index, 'locking'] = 'True'
         now = datetime.now()
         unlocking_account = now + timedelta(hours=1)
         return unlocking_account
 
-    def unlock_account(self, index):
+    def unlock_account(self):
+
         un_lock_time = Login.locking_account(self)
         now = datetime.now()
         if un_lock_time >= now:
             self.locking = False
             df = pd.read_csv('users_information.csv')
             # updating the column value/data
+            index_list = df.index[(df['username'] == self.username)].tolist()
+            index = int(index_list[0])
             df.loc[index, 'locking'] = 'False'
             # writing into the file
             df.to_csv('users_information.csv', index=False)
-
-    @staticmethod
-    def check_locking(index):
-        if os.path.exists('users_information.csv'):
-            with open('users_information.csv', 'r') as file:
-                reader = csv.DictReader(file)
-                df = pd.DataFrame(reader)
-                locking = df.at[index, 'locking']
-        return locking
 
     def login(self):
 
         my_tuple = User.get_info_from_csvfile()
         users_list = my_tuple[1]
         password_list = my_tuple[2]
+        lock_list = my_tuple [3]
         if self.username not in users_list:
             logger.error(f'this username:{self.username} not exist!')
             return False
         else:
-            if not self.locking:
-                for i in range(len(users_list)):
-                    if users_list[i] == self.username:
-                        if password_list[i] == User.hash_method(self.password):
+            for i in range(len(users_list)):
+                if users_list[i] == self.username:
+                    if password_list[i] == User.hash_method(self.password):
+                        if lock_list[i] == 'False':
                             logger.info(f'"{self.username}" have successfully entered the program ')
                             return True
                         else:
-                            print('the password is not correct.you can try 2 more times.')
-                            if Login.incorrect_password(self):
-                                logger.info(f'"{self.username}" have successfully entered the program ')
-                                return True
-                            else:
-                                # if user can't enter true password her/his account must be locked.
-                                Login.locking_account(self)
-                                logger.warning(f'this account :"{self.username}" is locked for 1 hour.because user'
-                                               f' entered incorrect password for 3 time.')
-                                return False
+                            Login.unlock_account(self)
+                            logger.warning('your account is locked')
+                    else:
+                        print('the password is not correct.you can try 2 more times.')
+                        if Login.incorrect_password(self):
+                            logger.info(f'"{self.username}" have successfully entered the program ')
+                            return True
+                        else:
+                            # if user can't enter true password her/his account must be locked.
+                            Login.locking_account(self)
+                            logger.warning(f'this account :"{self.username}" is locked for 1 hour.because user'
+                                           f' entered incorrect password for 3 time.')
+                            return False
 
     def incorrect_password(self):
         """
